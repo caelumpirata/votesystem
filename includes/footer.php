c65b4dca7c3e1928710db55b3b4b3abc2a62682f
caelumpirata <caelumpirata@gmail.com>
2022-04-21 15:50:35 +0530

    created git respository

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Voting System</b>
+      </div>
+      <strong> &copy; 2022 Brought To You By<a href="/">Evoting Projects</a></strong>
+    </div>
+    <!-- /.container -->
+</footer>
